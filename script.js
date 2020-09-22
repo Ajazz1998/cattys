00f8e4c519f0f362f834edf7890bc1e89ec3e39a
Ajay Bava <ajaybava1998@gmail.com>
2020-09-22 13:32:07 +0100

    commit

--- script.js
+++ script.js
@@ -12,3 +12,31 @@ function carousel() {
     x[myIndex-1].style.display = "grid";  
     setTimeout(carousel, 2000);
 }
+
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlide");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-white", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " w3-white";
+}
